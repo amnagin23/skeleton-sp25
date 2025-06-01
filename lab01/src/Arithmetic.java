d413fde04cafdc951e7133ea11baaeed8f4bd638
zhengjunjie <2768211071@qq.com>
2025-06-01 14:02:18 +0800

    Update Arithmetic.java

--- lab01/src/Arithmetic.java
+++ lab01/src/Arithmetic.java
@@ -20,7 +20,7 @@ public class Arithmetic {
      * @return Sum of a and b
      * */
     public static int sum(int a, int b) {
-        return a * b;
+        return a + b;
     }
 
     public static void main(String[] args) {
@@ -31,6 +31,5 @@ public class Arithmetic {
 
         System.out.println("The product of " + num1 + " and " + num2 + " is: " + product(num1, num2));
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum(num1, num2));
-        System.out.println("e");
     }
 }
